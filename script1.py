9056c34d398a3120666f6346e7b8e3979ca235c3
Suresh Rajasekar <suresh.rajasekar89@gmail.com>
2024-01-15 10:19:03 +0530

    ashok changes updated

--- script1.py
+++ script1.py
@@ -1,8 +1,8 @@
-print("script 1 from main changed LINE 1")#Old Code
-print("script 1 from main changed LINE 2")#Old Code
-print("script 1 from main changed LINE 3")#Old Code
-print("script 1 from main changed LINE 4")#Old Code
-print("script 1 from main changed LINE 5")#Old Code
+print("script 1 from main changed LINE 1")#new ashok Code
+print("script 1 from main changed LINE 2")#new ashok Code
+print("script 1 from main changed LINE 3")#new ashok Code
+print("script 1 from main changed LINE 4")#new ashok Code
+print("script 1 from main changed LINE 5")#new ashok Code
 
 print("script 1 from main changed LINE 6")#Old Code
 print("script 1 from main changed LINE 7")#Old Code
